d1bd19647bb15b07b8287e5c41637a5022e38fd0
FAITH OLUWAFIKAYOMI ALABETUTU <52827972+Ericpane@users.noreply.github.com>
2026-09-05 18:04:06 +0100

    Delete campusride_vercel_postgres_ready/campusride_fix directory

--- campusride_vercel_postgres_ready/campusride_fix/campusride/__init__.py
+++
@@ -1,1 +0,0 @@
-"""CampusRide Django project package."""
--- campusride_vercel_postgres_ready/campusride_fix/rides/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, CampusLocation, ActiveTrip, RideRequest, TripHistory
-
-admin.site.register(Profile)
-admin.site.register(CampusLocation)
-admin.site.register(ActiveTrip)
-admin.site.register(RideRequest)
-admin.site.register(TripHistory)
